8edd154d23c7c3898018b8928cdd6bb02b28d844
Eduardo Habkost <ehabkost@redhat.com>
2018-08-22 09:19:05 -0300

    ignore node_modules on deploy
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- habkost-net/deploy.sh
+++ habkost-net/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 make
 JEKYLL_ENV=production bundle exec jekyll build
-rsync -zva --delete-after _site/ ehabkost1@george-read.dreamhost.com:habkost.net/
+rsync -zva --exclude node_modules --delete-after _site/ ehabkost1@george-read.dreamhost.com:habkost.net/
